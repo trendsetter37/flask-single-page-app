d261e90580db9263bfa3ab0d90c171c2fd449392
Javis Sullivan <JavisSullivan@gmail.com>
2015-06-23 19:13:57 -0400

    basic calc

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/hello')
+#@app.route('/')
+def hello():
 	return "Hello, World"
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+	if request.method == 'POST':
+		value_one = int(request.form['number-one'])
+		value_two = int(request.form['number-two'])
+		total = value_one + value_two
+		return render_template('index.html',value=total)
+	else:
+		return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
