21617e0206ded7ff663e37a8fcd249751eae437b
Javis Sullivan <JavisSullivan@gmail.com>
2015-06-24 01:03:42 -0400

    github api functionality

--- app/app.py
+++ app/app.py
@@ -1,23 +1,40 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
+
+GITHUB_URI = "https://api.github.com/search/users?"
 
 @app.route('/hello')
 #@app.route('/')
 def hello():
 	return "Hello, World"
 
+# @app.route('/', methods=["GET", "POST"])
+# def home():
+# 	if request.method == 'POST':
+# 		value_one = int(request.form.get('first'))
+# 		value_two = int(request.form.get('second'))
+# 		total = value_one + value_two
+# 		data = {"total":str(total)}
+# 		return jsonify(data)
+		
+# 	else:
+# 		return render_template('index.html')
+
 @app.route('/', methods=["GET", "POST"])
 def home():
 	if request.method == 'POST':
-		value_one = int(request.form.get('first'))
-		value_two = int(request.form.get('second'))
-		total = value_one + value_two
-		data = {"total":str(total)}
-		return jsonify(data)
+		# User imports
+		value_one = str(request.form.get('first'))
+		value_two = str(request.form.get('second'))
+		# api call
+		search_params = "q=location:{}+language:{}".format(value_one, value_two)
+		url = GITHUB_URI + search_params
+		response_dict = requests.get(url).json()
 		
-	else:
-		return render_template('index.html')
+		return jsonify(response_dict)
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- app/static/main.js
+++ app/static/main.js
@@ -2,10 +2,9 @@ $(document).ready(function () {
 	console.log("readayyy");
 	$('form').on("submit", function() {
 		console.log("The form has been submitted");
-		var valueOne = $("input[name='number-one']").val();
-		var valueTwo = $("input[name='number-two']").val();
-		console.log("Value one: " + valueOne + "\n" + "Value two: "
-			+ valueTwo)
+		var valueOne = $("input[name='location']").val();
+		var valueTwo = $("input[name='language']").val();
+		console.log(valueOne, valueTwo);
 
 		// ajax to the backend
 		$.ajax({
@@ -14,12 +13,20 @@ $(document).ready(function () {
 			data: { first: valueOne, second: valueTwo},
 			success: function(results) {
 				console.log(results);
-				$('#results').html(results.total);
-				$('input[name="number-one"]').val("");
-				$('input[name="number-one"]').focus();
-				$('input[name="number-two"]').val("");
+				results.items.forEach(function(user) {
+					var img = '<img src="' + user.avatar_url + '" width="200" height="200">'; 
+					$('#results').append('<br>');			
+					$('#results').append(img);
+					$('#results').append('<br><a href="' + user.html_url + '">' +
+					user.login + '</a>')
+					$('#results').append('<br><br>');
+				});
+				
+				$('input').val("");
+				
 			},
 			error: function(error) {
+				alert("There was an error");
 				console.log(error)
 			}
 		});
